01977e43c851b76525768c453780b258c280b2f9
CUANY Simon <Simon.CUANY@cpnv.ch>
2019-11-21 11:13:13 +0100

    Server php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>TCHO</h1>
+<?php
+/*
+   * Author : Simon Cuany
+   * Date   : 20.11.2019
+   * Hour   : 16:40
+
+*/
+$x = 12;
+$x = "Hello";
+echo "<ul>";
+for ($i = 1; $i < 10; $i++) {
+    echo "<LI>Produit.$i</li>";
+}
+echo "</ul>"
+?>
+
+</body>
+</html>
+
