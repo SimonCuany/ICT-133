00122df0c49ad9a510a4e9ae3f832686fdec4712
SimonCuany <47849679+SimonCuany@users.noreply.github.com>
2019-12-19 10:14:35 +0100

    mvc

---
+++ Gabarit/Index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<head>
+    <meta charset="UTF-8">
+    <title>Gabarit</title>
+</head>
+<body>
+
+
+<?php
+/**
+ * Title  : Gabarit
+ * User   : Simon.CUANY
+ * Date   : 13.12.2019
+ * Time   : 13:45
+ */
+?>
+</body>
+</html>
+
